39a5e965de8ebb26078bee107f7fd9fc6c5de13a
jonathan vanasco <jonathan@2xlp.com>
2020-10-20 18:18:36 -0400

    v0.1.3

--- setup.py
+++ setup.py
@@ -6,17 +6,16 @@ import re
 from setuptools import setup
 from setuptools import find_packages
 
-try:
-    here = os.path.abspath(os.path.dirname(__file__))
-    README = open(os.path.join(here, "README.md")).read()
-    README = README.split("\n\n", 1)[0] + "\n"
-except:
-    README = ""
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+long_description = (
+    description
+) = "Standardizes server `fork` events into Pyrmamid events"
+with open(os.path.join(HERE, "README.md")) as r_file:
+    long_description = r_file.read()
 
 # store version in the init.py
-with open(
-    os.path.join(os.path.dirname(__file__), "pyramid_forksafe", "__init__.py")
-) as v_file:
+with open(os.path.join(HERE, "pyramid_forksafe", "__init__.py")) as v_file:
     VERSION = re.compile(r'.*__VERSION__ = "(.*?)"', re.S).match(v_file.read()).group(1)
 
 
@@ -37,8 +36,9 @@ setup(
     url="https://github.com/jvanasco/pyramid_forksafe",
     author="Jonathan Vanasco",
     author_email="jonathan@findmeon.com",
-    description="provides for a unified fork events",
-    long_description=README,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Intended Audience :: Developers",
         "Framework :: Pyramid",
@@ -47,7 +47,7 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
-    keywords="web pyramid fork",
+    keywords="web pyramid fork uwsgi nginx",
     license="MIT",
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
